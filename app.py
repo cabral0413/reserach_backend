0e740c8e123a4b04211031052af29cd5825dba4e
cabral0413 <nipunicabral27@gmail.com>
2024-10-16 02:30:32 +0530

    Initial commit for deployment

--- app.py
+++ app.py
@@ -91,5 +91,4 @@ def get_venom_status(class_name):
     return venom_status_map.get(class_name, 'Unknown')
 
 if __name__ == '__main__':
-    # Set debug to False for production
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
